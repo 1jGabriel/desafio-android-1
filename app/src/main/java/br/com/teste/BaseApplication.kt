4a9678b680a99fa93788dac1e5f6fc03be8a4f06
João Sousa <joaogabriel.sousa@beblue.com.br>
2020-01-11 11:05:01 -0300

    added font to project

--- app/src/main/java/br/com/teste/BaseApplication.kt
+++ app/src/main/java/br/com/teste/BaseApplication.kt
@@ -3,8 +3,28 @@ package br.com.teste
 import br.com.teste.common.di.DaggerAppComponent
 import dagger.android.AndroidInjector
 import dagger.android.support.DaggerApplication
+import io.github.inflationx.calligraphy3.CalligraphyConfig
+import io.github.inflationx.calligraphy3.CalligraphyInterceptor
+import io.github.inflationx.viewpump.ViewPump
 
 class BaseApplication : DaggerApplication() {
+
+    override fun onCreate() {
+        super.onCreate()
+        ViewPump.init(
+            ViewPump.builder()
+                .addInterceptor(
+                    CalligraphyInterceptor(
+                        CalligraphyConfig.Builder()
+                            .setDefaultFontPath("fonts/Nunito-Regular.ttf")
+                            .setFontAttrId(R.attr.fontPath)
+                            .build()
+                    )
+                )
+                .build()
+        )
+    }
+
     override fun applicationInjector(): AndroidInjector<out DaggerApplication> =
         DaggerAppComponent
             .factory()
