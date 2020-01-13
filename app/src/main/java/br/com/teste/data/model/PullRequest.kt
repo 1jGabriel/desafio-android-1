21cfe1ecc3fbf1c690489c7f62c592e97032f07f
João Sousa <joaogabriel.sousa@beblue.com.br>
2020-01-13 10:34:16 -0300

    Added default values

--- app/src/main/java/br/com/teste/data/model/PullRequest.kt
+++ app/src/main/java/br/com/teste/data/model/PullRequest.kt
@@ -6,15 +6,15 @@ import java.util.*
 
 data class PullRequest(
     @SerializedName("body")
-    val body: String,
+    val body: String = "",
     @SerializedName("created_at")
-    val createdAt: Date,
+    val createdAt: Date = Date(),
     @SerializedName("html_url")
-    val htmlUrl: String,
+    val htmlUrl: String = "",
     @SerializedName("title")
-    val title: String,
+    val title: String = "",
     @SerializedName("url")
-    val url: String,
+    val url: String = "",
     @SerializedName("user")
     val user: User = User()
 )
